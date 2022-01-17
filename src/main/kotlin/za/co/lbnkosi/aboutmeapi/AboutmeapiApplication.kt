67fe8eed6ba87f4c0666e49dc1987eb18e285ff2
Lebogang Nkosi <lebogang@lbnkosi.co.za>
2022-01-17 07:59:52 +0200

    Initial commit

---
+++ src/main/kotlin/za/co/lbnkosi/aboutmeapi/AboutmeapiApplication.kt
@@ -0,0 +1,11 @@
+package za.co.lbnkosi.aboutmeapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AboutmeapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AboutmeapiApplication>(*args)
+}
---
+++ src/test/kotlin/za/co/lbnkosi/aboutmeapi/AboutmeapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package za.co.lbnkosi.aboutmeapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AboutmeapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
